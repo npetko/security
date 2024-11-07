11aa0cb386e1192d56bb68d63b0b91f7a94ea5c6
npetko <npnikolapetkovic@gmail.com>
2024-11-07 01:01:47 +0100

    Add database

---
+++ database.js
@@ -0,0 +1,9 @@
+const users = [
+   { id: 1, username: 'admin', password: 'admin' },
+   { id: 2, username: 'nikola', password: 'nikola' },
+   { id: 3, username: 'luka', password: 'luka' },
+   { id: 4, username: 'laura', password: 'laura' },
+   { id: 5, username: 'kristina', password: 'kristina' }
+];
+
+module.exports = { users };
